3f86afa8ac15a7d1c916d3f6b2a10feb5c9565bf
Britteany <miszbritteany@gmail.com>
2024-03-09 15:48:52 -0500

    styled homepage

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React + Vite
 
+Photo by Tina Simakova from Pexels: https://www.pexels.com/photo/neon-of-coffee-bar-10312343/
+
+Photo by Negative Space: https://www.pexels.com/photo/a-close-up-shot-of-a-roasted-coffee-beans-34085/
+
 
--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -1,33 +1,31 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../styles/Coffee.css";
 
 const Coffee = () => {
-    const[coffeeData, setCoffeeData] = useState(null)
-    const { id } = useParams();
-    console.log(id);
+  const [coffeeData, setCoffeeData] = useState(null);
+  const { id } = useParams();
+  console.log(id);
 
-    useEffect(() => {
-        async function getData() {
-          setCoffeeData("loading coffee");
-          let data = await fetch(
-            `https://fake-coffee-api.vercel.app/api/${id}`
-          );
-          let pureData = await data.json();
-          setCoffeeData(pureData);
-          console.log(pureData)
-        }
-        getData();
-      }, []);
-      
+  useEffect(() => {
+    async function getData() {
+      setCoffeeData("loading coffee");
+      let data = await fetch(`https://fake-coffee-api.vercel.app/api/${id}`);
+      let pureData = await data.json();
+      setCoffeeData(pureData);
+      console.log(pureData);
+    }
+    getData();
+  }, [id]);
 
   return (
     <div>
       <h1>COFFEE PAGE</h1>
-    {coffeeData === null ? (
+      {coffeeData === null ? (
         <p>Loading coffee...</p>
       ) : (
-        (JSON.stringify(coffeeData))
-      )}     
+        <pre>{JSON.stringify(coffeeData)}</pre>
+      )}
     </div>
   );
 };
